6553d91249aa6f794842bf4852ff2f0ab35ad597
devttheus <ttheus.rlima@gmail.com>
2022-08-20 20:47:21 -0300

    Finalisado as funções e concluído a etapa para a interação do usuário

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 function start(){
-  console.log("start function");
+  //console.log("start function");
+  var buttonCalculateImc = document.querySelector("#button-calculate-imc");
+  buttonCalculateImc.addEventListener("click", handleButtonClick);  
+
+  handleButtonClick();
 }
 
 function calculateImc(weight, height){
   return weight / (height * height);
+}
+
+function handleButtonClick(){
+  //console.log("clicked");
+  var inputweight = document.querySelector("#input-weight");
+  var inputheight = document.querySelector("#input-height");
+  var imcResult = document.querySelector("#imc-result");
+
+  //Number: serve para converter strings
+  var weight = Number(inputweight.value);
+  var height = Number(inputheight.value);
+
+  var imc = calculateImc(weight,height);
+  var formateImc = imc.toFixed(2).replace(".", ",");
+
+  //textContent é para receber o conteúdo.
+  imcResult.textContent = formateImc;
+  //console.log(imc);
 }
 
 start();
