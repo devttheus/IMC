1fdbf443f28dd90764666f2dc5b8bcea8e597821
devttheus <ttheus.rlima@gmail.com>
2022-09-05 18:48:26 -0300

    Adicionando uma estrutura de decisão para ver qual faixa de peso a pessoa está

--- js/script.js
+++ js/script.js
@@ -26,6 +26,35 @@ function handleButtonClick(){
   //textContent é para receber o conteúdo.
   imcResult.textContent = formateImc;
   //console.log(imc);
+
+  var dateWeight = null;
+  if(imc >= 16 && imc <= 16.9){
+    dateWeight = "Muito abaixo do peso";
+  }
+  else if(imc >= 17 && imc <= 18.4){
+    dateWeight = "Abaixo do peso";
+  }
+  else if(imc >= 18.5 && imc <= 24.9){
+    dateWeight = "Peso normal";
+  }
+  else if(imc >= 25 && imc <= 29.9){
+    dateWeight = "Acima do peso";
+  }
+  else if(imc >= 30 && imc <= 34.9){
+    dateWeight = "Obesidade grau I";
+  }
+  else if(imc >= 35 && imc <= 40){
+    dateWeight = "Obesidade grau II";
+  }
+  else if(imc >= 40 ){
+    dateWeight = "Obesidade grau III";
+  }else{
+    dateWeight = "valor invalido";
+  }
+
+  var dateImc = document.querySelector("#date-imc");
+  dateImc.textContent = dateWeight;
 }
+
 
 start();
